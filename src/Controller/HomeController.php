7c8d5ac6508c316abc34873e798960847ccc35c0
ThomasFortrie <thomas.fortrie@hotmail.fr>
2024-05-17 11:38:05 +0200

    Appel BDD liste materiel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\MatosRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(MatosRepository $matosRepository): Response
     {
+
+    $allMatos = $matosRepository->findAll();
+    // dd($allMatos);
+
         return $this->render('home.html.twig', [
-            'controller_name' => 'HomeController',
+            'materiels' => $allMatos
         ]);
     }
 }
