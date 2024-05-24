51ed23b94b34825d6b39dc48d03707fb2b507814
ThomasFortrie <thomas.fortrie@hotmail.fr>
2024-05-24 16:26:08 +0200

    fonction plus un activée

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Matos;
 use App\Repository\MatosRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
@@ -15,11 +17,28 @@ class HomeController extends AbstractController
     public function index(MatosRepository $matosRepository): Response
     {
 
-    $allMatos = $matosRepository->findAll();
-     // dd($allMatos);
+        $allMatos = $matosRepository->findAll();
+        // dd($allMatos);
 
         return $this->render('home.html.twig', [
             'materiels' => $allMatos
         ]);
+    }
+
+    #[Route('/plus/{id}', name: 'plusOne', requirements: ['id' => '\d+'])]
+    public function updatePlusOne(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $product = $entityManager->getRepository(Matos::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'Materiel innexistant ! Probleme !'
+            );
+        }
+        $q = $product->getQuantite() +1 ;
+        $product->setQuantite($q);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
     }
 }
