30f38099d719b65b77a177918d4cc127d9dddc37
ThomasFortrie <thomas.fortrie@hotmail.fr>
2024-05-31 14:59:23 +0200

    mise en forme du form sur la page d'accueil

--- src/Form/MatosType.php
+++ src/Form/MatosType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Matos;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,10 +15,13 @@ class MatosType extends AbstractType
         $builder
             ->add('nom')
             ->add('quantite')
-            ->add('majDate', null, [
-                'widget' => 'single_text',
+          //  ->add('majDate', null, [
+            //    'widget' => 'single_text',
+          //  ])
+            //->add('lastModif')
+            ->add('save', SubmitType::class,[
+                'label' => 'Soumettre'
             ])
-            ->add('lastModif')
         ;
     }
 
