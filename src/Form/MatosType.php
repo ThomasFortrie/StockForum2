aa0266d56d71076d4e91dbc288dbb74a189eb302
ThomasFortrie <thomas.fortrie@hotmail.fr>
2024-06-06 17:22:23 +0200

    ajout choix de l'user

--- src/Form/MatosType.php
+++ src/Form/MatosType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Matos;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,7 +19,12 @@ class MatosType extends AbstractType
           //  ->add('majDate', null, [
             //    'widget' => 'single_text',
           //  ])
-            //->add('lastModif')
+            ->add('lastModif', ChoiceType::class, [
+                'choices' => [
+                    'Thomas' => 'Thomas',
+                    'Pierre Luc' => 'Pierre Luc'
+                ]
+            ])
             ->add('save', SubmitType::class,[
                 'label' => 'Soumettre'
             ])
